868a0ef5ddb7224e6e650f606d75685897ab5890
BgWm <Bigwormsben@gmail.com>
2019-11-08 20:25:53 +0800

    style

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -3,6 +3,6 @@ _in = input("[Message To Encrpyt]:")
 Base = lambda x : ord('A') if x < ord('Z') else ord('a')
 Shift = lambda x, y: chr((x - Base(x) + y) % 26 + Base(x))
 ret = list(''.join(list(Shift(ord(ch), i) for ch in _in)) for i in range(0,26))
-print("\n", ret, "\n")
 
+print("\n", ret, "\n")
 print("[end]")
